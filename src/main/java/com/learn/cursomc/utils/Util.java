2eabbc159ad4cce02e04e110c2d62401f233b53c
dimas-flj <dimasflj@gmail.com>
2019-11-19 14:50:20 -0300

    Validacao customizada de CPF e CNPJ na insercao de Cliente

--- src/main/java/com/learn/cursomc/utils/Util.java
+++ src/main/java/com/learn/cursomc/utils/Util.java
@@ -61,4 +61,155 @@ public class Util {
 		
 		return true;
 	}
+	
+	/**
+	 * Efetua a consistencia do CPF.
+	 * 
+	 * @param dado
+	 *        String contendo o dado do CPF.
+	 * 
+	 * @return True caso o CPF esteja valido.
+	 */
+	public static boolean isValidCPF(String cpf) {
+		if (!isValidString(cpf)) {
+			return false;
+		}
+		
+		cpf = getDigitos(cpf);
+		if (cpf.length() != 11) {
+			return false;
+		}
+		
+		if (!cpf.substring(9).equals(calculaDVCPF(cpf.substring(0, 9)))) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Consiste o numero do CNPJ.
+	 * 
+	 * @param cnpj
+	 *        Numero do CNPJ a ser verificado, contendo apenas os digitos.
+	 */
+	public static boolean isValidCNPJ(String cnpj) {
+		if (!isValidString(cnpj)) {
+			return false;
+		}
+		
+		cnpj = getDigitos(cnpj);
+		if (cnpj.length() != 14) {
+			return false;
+		}
+		
+		if (!cnpj.substring(12).equals(calculaDVCNPJ(cnpj.substring(0, 12)))) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Retorna apenas os dígitos existentes em uma string.
+	 * 
+	 * @param in
+	 *        String a ser formatada
+	 * @return Retorna apenas os dígitos da String
+	 */
+	public static String getDigitos(String in) {
+		String ret = null;
+		if (!isNull(in)) {
+			StringBuffer sb = new StringBuffer();
+			for(int i = 0; i < in.length(); i ++) {
+				if (Character.isDigit(in.charAt(i))) {
+					sb.append(in.charAt(i));
+				}
+			}
+			ret = sb.toString();
+		}
+		
+		return ret;
+	}
+	
+	/**
+	 * Obtem o digito verificador de um determinado CPF.
+	 * 
+	 * @param cpf
+	 *        CPF sem os digitos verificadores (deve ter pelo menos 9 caracteres).
+	 * 
+	 * @return String contendo os digitos verificador do CPF.
+	 */
+	public static String calculaDVCPF(String cpf) {
+		if (isValidString(cpf)) {
+			cpf = getDigitos(cpf);
+			// calcula o primeiro digito verificador
+			int s1 = 0;
+			for(int i = 1; i <= 9; i ++) {
+				s1 += Character.digit(cpf.charAt(i - 1), 10) * (11 - i);
+			}
+			
+			int r1 = (s1 % 11);
+			int dv1 = (r1 < 2 ? 0 : (11 - r1));
+			
+			// calcula o segundo digito verificador
+			int s2 = (dv1 * 2);
+			for(int i = 1; i <= 9; i ++) {
+				s2 += Character.digit(cpf.charAt(i - 1), 10) * (12 - i);
+			}
+			
+			int r2 = (s2 % 11);
+			int dv2 = (r2 < 2 ? 0 : (11 - r2));
+			
+			// retorna os digitos transformados em string
+			return (Integer.toString(dv1) + Integer.toString(dv2));
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Obtem o digito verificador de um determinado CNPJ
+	 * 
+	 * @param sCNPJ
+	 *        CNPJ sem os digitos verificadores (deve ter pelo menos 12 caracteres)
+	 * 
+	 * @return String contendo os digitos verificador do CNPJ
+	 */
+	public static String calculaDVCNPJ(String sCNPJ) {
+		if (isValidString(sCNPJ)) {
+			sCNPJ = getDigitos(sCNPJ);
+			// calcula o primeiro digito verificador
+			int s1 = 0;
+			
+			for(int i = 1; i <= 4; i ++) {
+				s1 += Character.digit(sCNPJ.charAt(i - 1), 10) * (6 - i);
+			}
+			
+			for(int i = 5; i <= 12; i ++) {
+				s1 += Character.digit(sCNPJ.charAt(i - 1), 10) * (14 - i);
+			}
+			
+			int r1 = (s1 % 11);
+			int dv1 = (r1 < 2 ? 0 : (11 - r1));
+			
+			// calcula o segundo digito verificador
+			int s2 = (dv1 * 2);
+			for(int i = 1; i <= 5; i ++) {
+				s2 += Character.digit(sCNPJ.charAt(i - 1), 10) * (7 - i);
+			}
+			
+			for(int i = 6; i <= 12; i ++) {
+				s2 += Character.digit(sCNPJ.charAt(i - 1), 10) * (15 - i);
+			}
+			
+			int r2 = (s2 % 11);
+			int dv2 = (r2 < 2 ? 0 : (11 - r2));
+			
+			// retorna os digitos transformados em string
+			return Integer.toString(dv1) + Integer.toString(dv2);
+		}
+		
+		return null;
+	}
 }
