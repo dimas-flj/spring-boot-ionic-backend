bc1b968c546bde41beed32dcce82a46be1f2c8b7
dimas-flj <dimasflj@gmail.com>
2019-11-20 11:09:10 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/learn/cursomc/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.learn.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.learn.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDataBase() throws ParseException {
+		System.out.println("::::::::::::::::::::::::::::: STRATEGY = " + strategy);
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		dbService.instatiateTestDataBase();
+		
+		return true;
+	}
+}
