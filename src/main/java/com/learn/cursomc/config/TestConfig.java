0b9f1cb235ae33c2ae17ad3d76a01cdfa5595160
dimas-flj <dimasflj@gmail.com>
2019-11-22 12:17:00 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/learn/cursomc/config/TestConfig.java
+++ src/main/java/com/learn/cursomc/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.learn.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -17,9 +18,17 @@ public class TestConfig {
 	@Autowired
 	private DBService dbService;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDataBase() throws ParseException {
+		System.out.println("::::::::::::::::::::::::::::: STRATEGY = " + strategy);
+		if (!"create".equals(strategy)) {
+			return false;
+		}
 		dbService.instatiateTestDataBase();
+		
 		return true;
 	}
 	
---
+++ src/main/java/com/learn/cursomc/services/SmtpEmailService.java
@@ -0,0 +1,21 @@
+package com.learn.cursomc.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+public class SmtpEmailService extends AbstractEmailService {
+	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
+	
+	@Autowired
+	private MailSender mailSender;
+	
+	public void sendEmail(SimpleMailMessage msg) {
+		LOG.info("Enviando email ...");
+		LOG.info(msg.toString());
+		mailSender.send(msg);
+		LOG.info("Email enviado.");
+	}
+}
