9eb0f60c30cec45900bef2f32a071bae71f2b053
dimas-flj <dimasflj@gmail.com>
2019-11-24 13:43:50 -0300

    Implementando autenticação e geração do token JWT

--- src/main/java/com/learn/cursomc/config/SecurityConfig.java
+++ src/main/java/com/learn/cursomc/config/SecurityConfig.java
@@ -7,30 +7,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.learn.cursomc.security.JWTAuthenticationFilter;
+import com.learn.cursomc.security.JWTUtil;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
 	private Environment env;
 	
-	public static final String[] PUBLIC_MATCHES = {
-		"/h2-console/**"
-	};
+	@Autowired
+	private JWTUtil jwtUtil;
 	
-	public static final String[] PUBLIC_MATCHES_GET = {
-		"/produtos/**",
-		"/categorias/**",
-		"/clientes/**"
-	};
+	public static final String[] PUBLIC_MATCHES = {"/h2-console/**"};
+	
+	public static final String[] PUBLIC_MATCHES_GET = {"/produtos/**", "/categorias/**", "/clientes/**"};
 	
 	protected void configure(HttpSecurity http) throws Exception {
 		// configuracao para peculiaridade do DB em memoria H2
@@ -40,21 +45,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		// ativa o metodo abaixo corsConfigurationSource()
 		http.cors().
-		// desabilita protecao CSRF em sistemas stateless 
-		and().csrf().disable();
+			// desabilita protecao CSRF em sistemas stateless
+			and().csrf().disable();
 		
 		http.
-		// autoriza todos os caminhos do vetor PUBLIC_MATCHES
-		authorizeRequests().
-		antMatchers(HttpMethod.GET, PUBLIC_MATCHES_GET).permitAll().
-		antMatchers(PUBLIC_MATCHES).permitAll().
+			// autoriza todos os caminhos do vetor PUBLIC_MATCHES
+			authorizeRequests().
+				antMatchers(HttpMethod.GET, PUBLIC_MATCHES_GET).permitAll().
+				antMatchers(PUBLIC_MATCHES).permitAll().
+			
+			// exige autenticacao para caminhos nao existentes no vetor PUBLIC_MATCHES
+			anyRequest().authenticated();
 		
-		// exige autenticacao para caminhos nao existentes no vetor PUBLIC_MATCHES
-		anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		super.configure(http);
 		
 		// assegura que esse back-ende nao crie sessao de usuario
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
 	
 	@Bean
