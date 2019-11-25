2fe6c1db812495a7d533c0babb7b0004662231db
dimas-flj <dimasflj@gmail.com>
2019-11-25 20:48:38 -0300

    Esqueci a senha

--- src/main/java/com/learn/cursomc/services/EmailService.java
+++ src/main/java/com/learn/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.learn.cursomc.domain.Cliente;
 import com.learn.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -16,4 +17,7 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	void sendHtmlEmail(MimeMessage msg);
 	// VERSAO HTML ::::::::::::
+	
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
