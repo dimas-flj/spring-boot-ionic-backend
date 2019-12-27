25c15d55931b0cdbe320d627e49b7ca56a8fc665
dimas-flj <dimasflj@gmail.com>
2019-12-27 12:55:37 -0300

    Melhorias na classe com.learn.cursomc.security.JWTUtil 43. Autualizacao da versao do spring para 2.2.1

--- src/main/java/com/learn/cursomc/security/JWTUtil.java
+++ src/main/java/com/learn/cursomc/security/JWTUtil.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
@@ -17,8 +18,14 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
-@ConfigurationProperties("jwt")
 @EnableAutoConfiguration
+@ConfigurationProperties("jwt")
+@PropertySource(
+	{
+		"classpath:/application.properties",
+		"classpath:/application-${spring.profiles.active}.properties"
+	}
+)
 public class JWTUtil {
 	@Value("${jwt.secret}")
 	private String jwt_secret;
